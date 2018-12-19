bf49a6784946893b0e1a7b96d94ac4b3427591bf
robox <root@roboxv.pro>
2018-12-19 21:59:57 +0600

    Extract phone number

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        message_text = update.message.text
+        formatted_phone_number = ''.join([c for c in message_text if c.isnumeric()])[-11:]
+        result = f'https://wa.me/{formatted_phone_number}'
+        bot.sendMessage(chat_id=chat_id, text=result)
     return "ok"
